241fc27b31dcc1886f9b9fdc643c0a9b60b979b1
BIGJAY5000 <ocampo.616058@swl.k12.oh.us>
2023-11-08 09:39:46 -0500

    ioefiwnirwj

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Scanner scan = new Scanner(System.in);
+
+    //variables
+        String s;
+        String current;
+        int count;
+        int max = 0;
+        String maxLetter = "";
+        System.out.println("Please enter a string");
+        s = scan.nextLine();
+        for (int i = 0; i < s.length(); i++) {
+            current = s.substring(i, i + 1);
+            if (i == 0 || !s.substring(0, i).contains(current)) {
+                count = 1;
+                for (int j = i + 1; j < s.length(); j++) {
+                    if (current.equals(s.substring(j, j+1))) {
+                        count++;
+                        if (max < count) {
+                            max = count;
+                            maxLetter = current;
+                        }
+                    }
+
+                }
+            }
+        }
+        System.out.println(maxLetter + " - Frequency: " + max);
     }
 }
